c471de00996719e9508990577cecce6f09cf3a6c
tom_bra <thomas.brandner@edu.htl-klu.at>
2025-01-06 20:24:15 +0100

    Severläuft local

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+//COMMENT
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/index', name: 'app_index')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $hw = "Hello World!";
+        $day = date("l");
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+                    'hw' => $hw,
+            "day" => $day
         ]);
     }
 }
